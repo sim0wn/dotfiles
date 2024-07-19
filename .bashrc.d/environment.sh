9e7546f89bef12d4c1eb537cbbba004a96687882
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-07-18 22:34:53 -0300

    adding git configuration dotfile

--- .bashrc.d/environment.sh
+++ .bashrc.d/environment.sh
@@ -9,3 +9,6 @@ export PATH="$HOME/.local/share/go/bin/:$PATH"
 # customize default npm directories
 export npm_config_userconfig=~/.config/npm
 export npm_config_cache=~/.cache/npm
+
+# customize git configuration directory
+export GIT_CONFIG="$HOME/.config/git/config"
