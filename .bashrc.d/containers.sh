f3d85209098b56403e82d3dcc99f6a44d4ae4e42
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-10-26 22:32:33 -0300

    :wrench: chore: make aliases function and remove insecure container flags

--- .bashrc.d/containers.sh
+++ .bashrc.d/containers.sh
@@ -2,42 +2,66 @@
 # --- Document Processing Tools ---
 # ---------------------------------
 
-alias pandoc='podman run --rm -v "${PWD}:/data:z" pandoc/latex'
+function pandoc() {
+  podman run --rm \
+    --volume="${PWD}:/data:Z" \
+    pandoc/latex "$@"
+}
+
+export -f pandoc
 
 # ------------------------------
 # --- Security Testing Tools ---
 # ------------------------------
-alias cewl='podman run -it --rm -v "${PWD}:/host" ghcr.io/digininja/cewl'
+function caido() {
+  podman run --rm \
+    --volume="${HOME}/.local/share/caido:/home/caido/.local/share/caido:Z" \
+    caido/caido:latest "$@"
+}
 
-alias caido='podman run --rm -p 8080:8080 --userns=keep-id --user root -v "$HOME/.local/share/caido:/home/caido/.local/share/caido" caido/caido:latest'
+function cewl() {
+  podman run --interactive --rm \
+    --volume="${PWD}:/host" \
+    ghcr.io/digininja/cewl "$@"
+}
 
 function feroxbuster () {
-  podman run --rm --init --interactive --userns=keep-id \
-    --volume="$HOME/.config/feroxbuster:/root/.config/feroxbuster:z" \
-    --volume="$PWD:/data:z" \
+  podman run --rm --init --interactive \
+    --volume="${HOME}/.config/feroxbuster:/root/.config/feroxbuster:Z" \
+    --volume="${PWD}:/data:Z" \
     --workdir /data \
     docker.io/epi052/feroxbuster:latest "$@"
 }
 
+function jwt_tool() {
+  podman run --rm --interactive \
+    --volume="${PWD}:/tmp:Z" \
+    --volume="${HOME}/.config/jwt_tool:/root/.jwt_tool:Z" \
+    ticarpi/jwt_tool:latest "$@"
+}
+
 function mitmproxy() {
   podman run --rm --interactive --tty \
-    --publish 8080:8080 --publish "127.0.0.1:8081:8081" \
-    --userns=keep-id --user root \
-    --volume="$HOME/.config/mitmproxy:/home/mitmproxy/.mitmproxy:z" \
-    --volume="$PWD:/data:z" \
+    --publish "8080:8080" \
+    --publish "127.0.0.1:8081:8081" \
+    --volume="${HOME}/.config/mitmproxy:/home/mitmproxy/.mitmproxy:Z" \
+    --volume="${PWD}:/data:Z" \
     --workdir /data \
     docker.io/mitmproxy/mitmproxy "$@"
 }
 
 function rustscan () {
-  podman run --interactive --tty --rm \
+  podman run --rm --interactive --tty \
     --cap-add=NET_ADMIN,NET_RAW,NET_BIND_SERVICE --privileged \
-    --workdir="/data" --volume="$PWD:/data:z" \
-    --user root --userns=keep-id \
+    --volume="${PWD}:/data:Z" \
+    --workdir="/data" \
     --name rustscan \
     docker.io/rustscan/rustscan:latest --no-config "$@"
 }
 
+export -f caido
+export -f cewl
 export -f feroxbuster
+export -f jwt_tool
 export -f mitmproxy
 export -f rustscan
