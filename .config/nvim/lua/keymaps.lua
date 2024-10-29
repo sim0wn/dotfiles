d60ad832cd6e1313393e27f9559c8bba4d651a49
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-10-29 13:12:08 -0300

    :wrench: chore: add keybinding to abort completions

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -10,8 +10,8 @@ local options = {
 -- Navigation
 vim.keymap.set('n', '<C-t>', ':tabnew<CR>', options)
 vim.keymap.set('n', '<C-q>', ':tabclose<CR>', options)
-vim.keymap.set('n', '<C-PageUp>', ':tabprevious<CR>', options)
-vim.keymap.set('n', '<C-PageDown>', ':tabnext<CR>', options)
+vim.keymap.set('n', '<S-H>', ':tabprevious<CR>', options)
+vim.keymap.set('n', '<S-L>', ':tabnext<CR>', options)
 
 -- Use arrows to resize
 vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', options)
@@ -23,5 +23,5 @@ vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', options)
 --- Visual Mode ---
 -------------------
 
-vim.keymap.set('v', '<', '<gv', opts)
-vim.keymap.set('v', '>', '>gv', opts)
+vim.keymap.set('v', '<', '<gv', options)
+vim.keymap.set('v', '>', '>gv', options)
